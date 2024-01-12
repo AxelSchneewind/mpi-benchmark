4be78735dcea9e166c3d2c746b4b6d9073a4142b
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-01-12 16:17:23 +0100

    simplify

--- benchmarks/bench_psend.c
+++ benchmarks/bench_psend.c
@@ -4,19 +4,18 @@
 void bench_psend(TestCase *test_case, Result *result, int comm_rank)
 {
 	// init
-	MPI_Request send_request;
-	MPI_Request recv_request;
+	MPI_Request request;
 
 	timer* timers;
 	timers_init(&timers, test_case, result);
 
 	if (comm_rank == 0)
 	{
-		MPI_Psend_init(test_case->buffer, test_case->partition_count, test_case->partition_size, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_INFO_ENV, &send_request);
+		MPI_Psend_init(test_case->buffer, test_case->partition_count, test_case->partition_size, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_INFO_ENV, &request);
 	}
 	else if (comm_rank == 1)
 	{
-		MPI_Precv_init(test_case->buffer, test_case->partition_count_recv, test_case->partition_size_recv, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_INFO_ENV, &recv_request);
+		MPI_Precv_init(test_case->buffer, test_case->partition_count_recv, test_case->partition_size_recv, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_INFO_ENV, &request);
 	}
 
 	// run
@@ -29,15 +28,15 @@ void bench_psend(TestCase *test_case, Result *result, int comm_rank)
 		{
 			timers_start_local(timers);
 
-			MPI_Start(&send_request);
+			MPI_Start(&request);
 
 			for (size_t p = 0; p < test_case->partition_count; p++) {
 				unsigned int partition_num = test_case->send_pattern[p];
 				work(test_case->partition_size);
-				MPI_Pready(partition_num, send_request);
+				MPI_Pready(partition_num, request);
 			}
 
-			MPI_Wait(&send_request, &result->send_status);
+			MPI_Wait(&request, &result->send_status);
 
 			timers_stop_local(timers);
 		}
@@ -45,8 +44,8 @@ void bench_psend(TestCase *test_case, Result *result, int comm_rank)
 		{
 			timers_start_local(timers);
 
-			MPI_Start(&recv_request);
-			MPI_Wait(&recv_request, &result->recv_status);
+			MPI_Start(&request);
+			MPI_Wait(&request, &result->recv_status);
 
 			timers_stop_local(timers);
 		}
