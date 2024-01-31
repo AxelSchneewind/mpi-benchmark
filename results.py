a72cf574732560743c6103f1a2fec49e7b21c8ff
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-01-31 18:33:41 +0100

    only display one-to-one sends by default

--- results.py
+++ results.py
@@ -7,15 +7,17 @@ import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 
-mode_names = ['Send', 'Isend', 'IsendTest', 'IsendThenTest', 'IsendTestall', 'Psend', 'Psend progress', 'PsendThreaded', 'custom Psend', 'WinSingle', 'Win']
+mode_names = ['Send', 'Isend', 'IsendTest', 'IsendThenTest', 'IsendTestall', 'Psend', 'PsendParrived', 'Psend progress', 'Psend progress threaded', 'PsendThreaded', 'custom Psend', 'WinSingle', 'Win']
 send_pattern_names = [ "linear", "linear inverse", "stride (128B)", "stride (1KB)", "random", "random burst (128B)" "random burst (1KB)" ]
 
 def plot(data, ax, mode, columns, domain=None, ylabel=None):
     mode_data = data[data['mode'] == mode] 
     mode_data.describe()
 
+    l = mode_names[mode]
     for pattern in data['send_pattern'].unique():
-        plot_data = mode_data[mode_data['send_pattern'] == pattern]
+        # only plot run with one-to-one relation between partitions
+        plot_data = mode_data[(mode_data['send_pattern'] == pattern) & (mode_data['partition_size'] == mode_data['partition_size_recv'])]
         for column in columns:
             xValues = plot_data['partition_size']
             yValues = plot_data[column]
@@ -25,7 +27,8 @@ def plot(data, ax, mode, columns, domain=None, ylabel=None):
             print(yValues.describe())
             print()
 
-            ax.plot(xValues, yValues, label=mode_names[mode] + ', ' + pattern)
+            ax.plot(xValues, yValues, label=l)
+            l = ''
 
     if ylabel !=None:
         ax.set_ylabel(ylabel)    
@@ -63,16 +66,19 @@ def plot_bandwiths(data):
     (fig, ax) = plt.subplots(4, 3)
     fig.suptitle('{0} (send/recv: {1:.3e})'.format('t_total', ref_value))
 
-    plot(data, ax[0,0], 0, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,1], 1, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,2], 2, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,0], 3, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,1], 4, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,2], 5, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,0], 6, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,1], 7, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,2], 8, column_names, domain, ylabel=ylabel)
-    plot(data, ax[3,2], 9, column_names, domain, ylabel=ylabel)
+    plot(data, ax[0,0],  0, column_names, domain, ylabel=ylabel)
+    plot(data, ax[0,1],  1, column_names, domain, ylabel=ylabel)
+    plot(data, ax[0,2],  2, column_names, domain, ylabel=ylabel)
+    plot(data, ax[1,0],  3, column_names, domain, ylabel=ylabel)
+    plot(data, ax[1,1],  4, column_names, domain, ylabel=ylabel)
+    plot(data, ax[1,2],  5, column_names, domain, ylabel=ylabel)
+    plot(data, ax[2,0],  6, column_names, domain, ylabel=ylabel)
+    plot(data, ax[2,1],  7, column_names, domain, ylabel=ylabel)
+    plot(data, ax[2,2],  8, column_names, domain, ylabel=ylabel)
+    plot(data, ax[3,0],  9, column_names, domain, ylabel=ylabel)
+    #plot(data, ax[3,1], 10, column_names, domain, ylabel=ylabel)
+    plot(data, ax[3,1], 11, column_names, domain, ylabel=ylabel)
+    plot(data, ax[3,2], 12, column_names, domain, ylabel=ylabel)
     plt.show()
 
 def plot_bandwiths_combined(data):
@@ -105,16 +111,19 @@ def plot_times(data):
     (fig, ax) = plt.subplots(4, 3)
     fig.suptitle('{0} (send/recv: {1:.3e})'.format('t_total', ref_value))
 
-    plot(data, ax[0,0], 0, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,1], 1, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,2], 2, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,0], 3, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,1], 4, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,2], 5, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,0], 6, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,1], 7, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,2], 8, column_names, domain, ylabel=ylabel)
-    plot(data, ax[3,2], 9, column_names, domain, ylabel=ylabel)
+    plot(data, ax[0,0],  0, column_names, domain, ylabel=ylabel)
+    plot(data, ax[0,1],  1, column_names, domain, ylabel=ylabel)
+    plot(data, ax[0,2],  2, column_names, domain, ylabel=ylabel)
+    plot(data, ax[1,0],  3, column_names, domain, ylabel=ylabel)
+    plot(data, ax[1,1],  4, column_names, domain, ylabel=ylabel)
+    plot(data, ax[1,2],  5, column_names, domain, ylabel=ylabel)
+    plot(data, ax[2,0],  6, column_names, domain, ylabel=ylabel)
+    plot(data, ax[2,1],  7, column_names, domain, ylabel=ylabel)
+    plot(data, ax[2,2],  8, column_names, domain, ylabel=ylabel)
+    plot(data, ax[3,0],  9, column_names, domain, ylabel=ylabel)
+    # plot(data, ax[3,1], 10, column_names, domain, ylabel=ylabel)
+    plot(data, ax[3,1], 11, column_names, domain, ylabel=ylabel)
+    plot(data, ax[3,2], 12, column_names, domain, ylabel=ylabel)
     plt.show()
 
 
