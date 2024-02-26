18e9860e9c0fe011420a4a4aed235d429c146bf2
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-02-26 18:32:22 +0100

    make plotting more modular

---
+++ plots.py
@@ -0,0 +1,89 @@
+#!python3
+
+import pandas as pd
+import sys
+
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+
+mode_names = [ 'Send' 'Isend', 'IsendTest', 'IsendThenTest', 'IsendTestall', 'CustomPsend', 'WinSingle', 'Win', 'Psend', 'PsendParrived', 'PsendProgress', 'PsendProgressThread', 'PsendThreaded']
+send_pattern_names = [ 'linear', 'linear inverse', 'stride (128B)', 'stride (1KB)', 'stride (16KB)', 'random', 'random burst (128B at a time)', 'random burst (1KB at a time)', 'random burst (16KB at a time)' ]
+
+# returns (xValues, yValues, pattern, mode, column) all partition_size -> column plots for the given modes and patterns
+def iter_results(data, modes = None, patterns = None, columns = None, filter = None):
+    if columns == None:
+        return
+
+    if filter != None:
+        data = filter(data)
+    
+    for pattern in patterns:
+        for mode in modes:
+            for column in columns:
+                plot_data = data[(data['send_pattern'] == pattern) & (data['mode'] == mode)] 
+                if len(plot_data) == 0:
+                    continue
+                xValues = plot_data['partition_size']
+                yValues = plot_data[column]
+                yield (xValues, yValues, mode, pattern, column)
+
+def plot(ax, x, y, domain=None, title=None, label=None, ylabel='Bandwidth [B/s]', xlabel='partition size [B]'):
+    ax.plot(x, y, label=label)
+
+    ax.grid()
+    if ylabel != None:
+        ax.set_ylabel(ylabel)    
+    if xlabel != None:
+        ax.set_xlabel(xlabel)
+
+    if title != None: 
+        ax.set_title(title)
+
+    if domain != None:
+        (xdomain, ydomain) = domain
+        if xdomain != None:
+            ax.set_xlim(xdomain)
+        if ydomain != None:
+            ax.set_ylim(ydomain)
+
+    ax.set_xscale(mpl.scale.LogScale(ax, base=2))
+
+    ax.legend()
+
+
+# def plot(data, ax, mode, columns, domain=None, ylabel=None):
+#     mode_data = data[data['mode'] == mode] 
+#     mode_data.describe()
+# 
+#     for pattern in data['send_pattern'].unique():
+#         # only plot run with one-to-one relation between partitions
+#         plot_data = mode_data[(mode_data['send_pattern'] == pattern) & (mode_data['partition_size'] == mode_data['partition_size_recv'])]
+#         for column in columns:
+#             xValues = plot_data['partition_size']
+#             yValues = plot_data[column]
+# 
+#             if len(yValues) > 0:
+#                 ax.plot(xValues, yValues, label=mode + ', ' + pattern)
+# 
+#     if ylabel !=None:
+#         ax.set_ylabel(ylabel)    
+#     else:
+#         ax.set_ylabel(columns[0])
+#     
+#     ax.grid()
+#     ax.set_xlabel('partition_size [B]')
+#     ax.set_xscale(mpl.scale.LogScale(ax, base=2))
+# 
+#     if domain == None:
+#         x = (min(ax.get_xlim()[0], min(xValues)), max(ax.get_xlim()[1], max(xValues)))
+#         y = (min(ax.get_ylim()[0], min(yValues)), max(ax.get_ylim()[1], max(yValues)))
+#         domain = (x, y)
+# 
+# 
+#     (xdomain, ydomain) = domain
+#     ax.set_ylim(ydomain)
+#     ax.set_xlim(xdomain)
+# 
+#     ax.legend()
+# 
--- results.py
+++ results.py
@@ -1,146 +1,49 @@
 #!/bin/python
 
-import pandas as pd
+import plots
 import sys
-
-import matplotlib as mpl
+import pandas as pd
 from matplotlib import pyplot as plt
 
 
-mode_names = ['Send', 'Isend', 'IsendTest', 'IsendThenTest', 'IsendTestall', 'custom Psend', 'WinSingle', 'Win', 'Psend', 'PsendParrived', 'Psend progress', 'Psend progress threaded', 'PsendThreaded' ]
-send_pattern_names = [ "linear", "linear inverse", "stride (128B)", "stride (1KB)", "random", "random burst (128B)" "random burst (1KB)" ]
+def plot_column(data, column_names = ['bandwidth'], ylabel='bandwidth [B/s]', domain=None):
+    rows = 4
+    cols = 4
+    (fig, ax) = plt.subplots(rows, cols)
 
-def plot(data, ax, mode, columns, domain=None, ylabel=None):
-    mode_data = data[data['mode'] == mode] 
-    mode_data.describe()
+    num_modes = 0
+    modes =  plots.mode_names
+    patterns =  plots.send_pattern_names
+    ax_per_mode = {}
 
-    l = mode_names[mode]
-    for pattern in data['send_pattern'].unique():
-        # only plot run with one-to-one relation between partitions
-        plot_data = mode_data[(mode_data['send_pattern'] == pattern) & (mode_data['partition_size'] == mode_data['partition_size_recv'])]
-        for column in columns:
-            xValues = plot_data['partition_size']
-            yValues = plot_data[column]
+    for (xValues, yValues, mode, pattern, column) in plots.iter_results(data, modes=modes, patterns=patterns, columns=column_names):
+        # next axis
+        if not mode in ax_per_mode.keys():
+            ax_per_mode[mode] = (num_modes // cols, num_modes % cols)
+            num_modes = num_modes + 1
 
-            print('Mode: ', mode_names[mode])
-            print('SendPattern: ', pattern)
-            print(yValues.describe())
-            print()
-
-            ax.plot(xValues, yValues, label=l)
-            l = ''
-
-    if ylabel !=None:
-        ax.set_ylabel(ylabel)    
-    else:
-        ax.set_ylabel(columns[0])
-    
-    ax.grid()
-    ax.set_xlabel('partition_size [B]')
-    ax.set_xscale(mpl.scale.LogScale(ax, base=2))
-
-    if domain == None:
-        x = (min(ax.get_xlim()[0], min(xValues)), max(ax.get_xlim()[1], max(xValues)))
-        y = (min(ax.get_ylim()[0], min(yValues)), max(ax.get_ylim()[1], max(yValues)))
-        domain = (x, y)
-
-
-    (xdomain, ydomain) = domain
-    ax.set_ylim(ydomain)
-    ax.set_xlim(xdomain)
-
-    ax.legend()
-
-
-def plot_bandwiths(data):
-    column_names = [ 'bandwidth' ]
-    ylabel = 'bandwidth [B/s]'
-    ydomain = (0, 30 * 2**30)
-    xdomain = (min(data['partition_size']), max(data['partition_size']))
-    domain = (xdomain, ydomain)
-
-    ref_value = data[data['mode'] == 0].iloc[0][column_names[0]]
-    if ref_value == None:
-        ref_value = 0
-
-    (fig, ax) = plt.subplots(4, 3)
-    fig.suptitle('{0} (send/recv: {1:.3e})'.format('t_total', ref_value))
-
-    plot(data, ax[0,0],  0, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,1],  1, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,2],  2, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,0],  3, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,1],  4, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,2],  5, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,0],  6, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,1],  7, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,2],  8, column_names, domain, ylabel=ylabel)
-    plot(data, ax[3,0],  9, column_names, domain, ylabel=ylabel)
-    #plot(data, ax[3,1], 10, column_names, domain, ylabel=ylabel)
-    plot(data, ax[3,1], 11, column_names, domain, ylabel=ylabel)
-    plot(data, ax[3,2], 12, column_names, domain, ylabel=ylabel)
-    plt.show()
-
-def plot_bandwiths_combined(data):
-    column_names = [ 'bandwidth' ]
-    domain = None
-
-    ref_value = data[data['mode'] == 0].iloc[0][column_names[0]]
-    if ref_value == None:
-        ref_value = 0
-
-    (fig, ax) = plt.subplots(1, 1)
-    fig.suptitle('{0} (send/recv: {1:.3e})'.format('bandwidth [B/s]', ref_value))
-
-    for i in range(len(mode_names)):
-        plot(data, ax, 0, column_names, domain)
+        if num_modes >= rows * cols:
+            break
+            
+        title = str(mode)
+        label = mode + ', ' + pattern
+        (a0, a1) = ax_per_mode[mode]
+        plots.plot(ax[a0, a1], xValues, yValues, domain, title=title, label=label)
 
     plt.show()
 
-def plot_times(data):
-    column_names = [ 't_total' ]
-    ylabel = 'total time [s]'
-    ydomain = None#(0, 30 * 2**30)
-    xdomain = (min(data['partition_size']), max(data['partition_size']))
-    domain = (xdomain, ydomain)
+def plot_column_combined(data, column_names=['bandwidth'], domain=None):
+    rows = 1
+    cols = 1
+    (fig, ax) = plt.subplots(rows, cols)
 
-    ref_value = data[data['mode'] == 0].iloc[0][column_names[0]]
-    if ref_value == None:
-        ref_value = 0
+    modes =  plots.mode_names
+    patterns =  plots.send_pattern_names
 
-    (fig, ax) = plt.subplots(4, 3)
-    fig.suptitle('{0} (send/recv: {1:.3e})'.format('t_total', ref_value))
-
-    plot(data, ax[0,0],  0, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,1],  1, column_names, domain, ylabel=ylabel)
-    plot(data, ax[0,2],  2, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,0],  3, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,1],  4, column_names, domain, ylabel=ylabel)
-    plot(data, ax[1,2],  6, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,0],  7, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,1],  8, column_names, domain, ylabel=ylabel)
-    plot(data, ax[2,2],  9, column_names, domain, ylabel=ylabel)
-    plot(data, ax[3,0], 10, column_names, domain, ylabel=ylabel)
-    plot(data, ax[3,1], 12, column_names, domain, ylabel=ylabel)
-    plt.show()
-
-
-def plot_times_combined(data):
-    column_names = [ 't_total']
-    domain = None
-
-    ref_value = data[data['mode'] == 0].iloc[0][column_names[0]]
-    if ref_value == None:
-        ref_value = 0
-
-    (fig, ax) = plt.subplots(1, 1)
-    fig.suptitle('{0} (send/recv: {1:.3e})'.format('t_total', ref_value))
-
-    for i in range(len(mode_names)):
-        plot(data, ax, i, column_names, domain)
-
-    plt.show()
-
+    for (xValues, yValues, mode, pattern, column) in plots.iter_results(data, modes=modes, patterns=patterns, columns=column_names):
+        title = column
+        label = mode + ', ' + pattern
+        plots.plot(ax[0, 0], xValues, yValues, domain, title=title, label=label)
 
 
 if __name__ == "__main__":
@@ -148,15 +51,16 @@ if __name__ == "__main__":
     combined = (len(sys.argv) > 2) and sys.argv[2] == 'combined'
     filename = sys.argv[3] if (len(sys.argv) > 3) else 'results.csv'
     
-    # TODO: set converters
     data = pd.read_csv(filename)
 
-    match (selected_value, combined):
-        case ('bandwidth', True):
-            plot_bandwiths_combined(data)
-        case ('bandwidth', False):
-            plot_bandwiths(data)
-        case ('time', True):
-            plot_times(data)
-        case ('time', False):
-            plot_times_combined(data)
+    if selected_value == 'time':
+        selected_value = 't_total'
+
+    domain = None
+    if selected_value == 'bandwidth':
+        domain = ((min(data['partition_size']), max(data['partition_size'])), (0, 30 * 2**30))
+
+    if combined:
+        plot_column_combined(data, [selected_value], domain=domain)
+    else:
+        plot_column(data, [selected_value], domain=domain)
