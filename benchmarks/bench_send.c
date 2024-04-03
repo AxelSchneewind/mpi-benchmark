3480e25255cfbf1ab9bf2b305b388c621917fc4f
Axel Schneewind <73230475+AxelSchneewind@users.noreply.github.com>
2024-04-03 11:53:08 +0200

    reformat code

--- benchmarks/bench_send.c
+++ benchmarks/bench_send.c
@@ -4,64 +4,62 @@
 
 void bench_send(TestCase *test_case, Result *result, int comm_rank)
 {
-	timers timers;
-	timers_init(&timers, TimerCount);
+    timers timers;
+    timers_init(&timers, TimerCount);
 
-	// warmup
-	if (comm_rank == 0) {
-		MPI_CHECK(MPI_Send(test_case->buffer, test_case->buffer_size, MPI_BYTE, 1, 0, MPI_COMM_WORLD));
-	} else {
-		MPI_CHECK(MPI_Recv(test_case->buffer, test_case->buffer_size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &result->recv_status));
-	}
+    // warmup
+    if (comm_rank == 0) {
+        MPI_CHECK(MPI_Send(test_case->buffer, test_case->buffer_size, MPI_BYTE, 1, 0, MPI_COMM_WORLD));
+    } else {
+        MPI_CHECK(MPI_Recv(test_case->buffer, test_case->buffer_size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &result->recv_status));
+    }
 
-	MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
-	timers_start(timers, Total);
+    MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
+    timers_start(timers, Total);
 
-	for (size_t i = 0; i < test_case->iteration_count; i++)
-	{
-		PreIteration;
-		if (comm_rank == 0)
-		{
-			MPI_Barrier(MPI_COMM_WORLD);
-			timers_start(timers, Iteration);
-			timers_start(timers, IterationStartToWait);
+    for (size_t i = 0; i < test_case->iteration_count; i++)
+    {
+        PreIteration;
+        if (comm_rank == 0)
+        {
+            MPI_Barrier(MPI_COMM_WORLD);
+            timers_start(timers, Iteration);
+            timers_start(timers, IterationStartToWait);
 
             #pragma omp parallel for num_threads(test_case->thread_count)
             for (int t = 0; t < test_case->thread_count; t++) {
-                for (size_t p = 0; p < test_case->partitions_per_thread; p++)
-                {
+                for (size_t p = 0; p < test_case->partitions_per_thread; p++) {
                     unsigned int partition_num = test_case->send_pattern[p + t * test_case->partitions_per_thread];				
-					work(test_case->partition_size);
-					MPI_CHECK(MPI_Send(test_case->buffer + test_case->partition_size * partition_num, test_case->partition_size, MPI_BYTE, 1, partition_num, MPI_COMM_WORLD));
-				}
-			}
+                    work(test_case->partition_size);
+                    MPI_CHECK(MPI_Send(test_case->buffer + test_case->partition_size * partition_num, test_case->partition_size, MPI_BYTE, 1, partition_num, MPI_COMM_WORLD));
+                }
+            }
 
-			timers_stop(timers, IterationStartToWait);
-			timers_stop(timers, Iteration);
-		}
-		else if (comm_rank == 1)
-		{
-			MPI_Barrier(MPI_COMM_WORLD);
-			timers_start(timers, Iteration);
-			timers_start(timers, IterationStartToWait);
+            timers_stop(timers, IterationStartToWait);
+            timers_stop(timers, Iteration);
+        }
+        else if (comm_rank == 1)
+        {
+            MPI_Barrier(MPI_COMM_WORLD);
+            timers_start(timers, Iteration);
+            timers_start(timers, IterationStartToWait);
 
             #pragma omp parallel for num_threads(test_case->thread_count)
             for (int t = 0; t < test_case->thread_count; t++) {
-                for (size_t p = 0; p < test_case->partitions_per_thread; p++)
-                {
+                for (size_t p = 0; p < test_case->partitions_per_thread; p++) {
                     unsigned int partition_num = test_case->recv_pattern[p + t * test_case->partitions_per_thread];				
-					MPI_CHECK(MPI_Recv(test_case->buffer + test_case->partition_size * partition_num, test_case->partition_size, MPI_BYTE, 0, partition_num, MPI_COMM_WORLD, &result->recv_status));
-				}
-			}
+                    MPI_CHECK(MPI_Recv(test_case->buffer + test_case->partition_size * partition_num, test_case->partition_size, MPI_BYTE, 0, partition_num, MPI_COMM_WORLD, &result->recv_status));
+                }
+            }
 
-			timers_stop(timers, IterationStartToWait);
-			timers_stop(timers, Iteration);
-		}
-	}
+            timers_stop(timers, IterationStartToWait);
+            timers_stop(timers, Iteration);
+        }
+    }
 
-	MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
-	timers_stop(timers, Total);
+    MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
+    timers_stop(timers, Total);
 
-	timers_store(timers, result);
-	timers_free(timers);
+    timers_store(timers, result);
+    timers_free(timers);
 };
